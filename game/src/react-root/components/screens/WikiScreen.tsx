0b8c7179f60a80857275d48c859211545cdcfd75
Roman Ulitin <kurt-77@mail.ru>
2024-06-13 19:46:16 +0500

    added buttons module

--- game/src/react-root/components/screens/WikiScreen.tsx
+++ game/src/react-root/components/screens/WikiScreen.tsx
@@ -3,6 +3,21 @@ import React from "react";
 import "./WikiScreen.css";
 
 const WikiScreen: React.FC = () => {
+    const changeCategory = (e) => {
+        e.preventDefault();
+        if (e.target.textContent === "мини игры") {
+            e.target.textContent = "модули";
+            e.target.parentElement.children[0].src =
+                "../../../../public/assets/ui/modules-icon.png";
+            e.target.parentElement.children[0].alt = "modules";
+        } else {
+            e.target.textContent = "мини игры";
+            e.target.parentElement.children[0].src =
+                "../../../../public/assets/ui/book-icon.png";
+            e.target.parentElement.children[0].alt = "mini games";
+        }
+    };
+
     return (
         <div className="wiki-container">
             <div className="wiki__wrapper">
@@ -10,11 +25,13 @@ const WikiScreen: React.FC = () => {
                     src="../../../../public/assets/ui/book-icon.png"
                     className="wiki__icon"
                     alt="game"
+                    width={75}
+                    height={65}
                 />
                 <button
                     className="button button--small"
                     type="button"
-                    onClick={submitData}
+                    onClick={changeCategory}
                 >
                     мини игры
                 </button>
@@ -24,6 +41,8 @@ const WikiScreen: React.FC = () => {
                     src="../../../../public/assets/ui/game-icon.png"
                     className="wiki__icon"
                     alt="game"
+                    width={85}
+                    height={65}
                 />
                 <button className="button button--small" type="button">
                     обучение
